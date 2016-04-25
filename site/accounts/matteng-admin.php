829c7b154aeed525f975ad6b65ea7dbc5aebf366
danielkremin <design@kremin.de>
2016-04-25 22:32:57 +0200

    Starterkit

---
+++ site/accounts/matteng-admin.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: matteng-admin
+email: design@kremin.de
+password: >
+  $2a$10$W3jJ6M8yf6ZD0vrSTDcBZeUXzhFinZyOYIZZ/AViGpvF7JZPdJMJ6
+language: de
+role: admin
