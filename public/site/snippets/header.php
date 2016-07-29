0ffedcf1cde8b945f417b13ad13d2969ae1d7154
danielkremin <design@kremin.de>
2016-07-29 22:27:50 +0200

    issue #27

--- public/site/snippets/header.php
+++ public/site/snippets/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+  <title><?php echo $site->title()->escape() ?> | <?php echo $page->title()->escape() ?></title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
 
