94b4c1f1bde5199fa57a210954b9f741263b1529
danielkremin <design@kremin.de>
2017-03-27 22:47:44 +0200

    Update Anleitung stationär

---
+++ public/site/snippets/partial_guide-Stationar.php
@@ -0,0 +1,7 @@
+<ul class="Services-guide-4">
+	<li><span class="icon-date"></span><p>Termin vereinbaren</p></li>
+	<li><span class="icon-house"></span><p>Im Laden vorbeikommen</p></li>
+	<li><span class="icon-stat"></span><p>Gerät da lassen, Ersatzteile werden bestellt</p></li>
+	<li><span class="icon-tool"></span><p>Reparatur erfolgt im Laden, wir melden uns</p></li>
+	<li><span class="icon-device"></span><p>Heiles Gerät wieder abholen, fertig</p></li>
+</ul>
